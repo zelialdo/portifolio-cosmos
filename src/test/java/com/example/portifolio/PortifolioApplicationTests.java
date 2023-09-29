36adcae1506349c9facaa5311794bbc4427ae47f
Zelialdo Sueldo <71799490+zelialdo@users.noreply.github.com>
2023-09-28 22:11:35 -0300

    commit inicial

---
+++ src/test/java/com/example/portifolio/PortifolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portifolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortifolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
